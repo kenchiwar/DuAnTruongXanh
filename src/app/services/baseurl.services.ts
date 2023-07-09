14c10fb4e6d5c5d2091dc79f735c620b28db3265
kenchiwar <huynhtienthang1264@gmail.com>
2023-07-09 17:43:47 +0700

    Update lần nữa

--- src/app/services/baseurl.services.ts
+++ src/app/services/baseurl.services.ts
@@ -1,11 +1,13 @@
 import { Injectable } from "@angular/core";
+import { UrlService } from "./url.services";
 
 
 
 
 @Injectable()
 export class UrlApi{
-   readonly  baseUrl  : string = "http://localhost:5138" ;
+  readonly urlService = new UrlService();
+   readonly  baseUrl  : string = this.urlService.baseUrl ;
    readonly  baseChuyenBayUrl:string = this.baseUrl+"/api/ChuyenBays";
    readonly  baseVeUrl:string = this.baseUrl+"/api/Ves";
 
