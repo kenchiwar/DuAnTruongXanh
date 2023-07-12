9ea4bb513a6aad0fa1fe2aebb82e1d431572511f
phantrungnghi <trungnghi202@gmail.com>
2023-07-12 14:31:43 +0700

    add css lan 1

--- src/app/admin/indeadmin.component.ts
+++ src/app/admin/indeadmin.component.ts
@@ -2,7 +2,8 @@ import { HttpClient } from "@angular/common/http";
 import { Component, OnInit, } from "@angular/core";
 import { Router } from "@angular/router";
 @Component({
-       templateUrl : './indexadmin.component.html'
+       templateUrl : './indexadmin.component.html',
+       styleUrls: ["./style.component.css"],
        
 })
 export class IndexAdminComponent implements OnInit {
