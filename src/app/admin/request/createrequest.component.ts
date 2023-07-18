0af98ec6815a4d151ec72c7b1b246631db117821
LeToaan <thaitoan553@gmail.com>
2023-07-18 16:55:01 +0700

    commit cua toan

--- src/app/admin/request/createrequest.component.ts
+++ src/app/admin/request/createrequest.component.ts
@@ -1,18 +1,46 @@
 import { HttpClient } from "@angular/common/http";
 import { Component, OnInit, } from "@angular/core";
-import { Router } from "@angular/router";
+import { FormGroup } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Account } from "src/app/models/account.model";
+import { Department } from "src/app/models/department.model";
+import { AccountService } from "src/app/services/account.service";
+import { DepartmentsServices } from "src/app/services/departments.service";
+import { RequestServices } from "src/app/services/request.service";
 @Component({
+        selector: 'app-request',
        templateUrl : './createrequest.component.html'
        
 })
 export class CreateRequestComponent implements OnInit {
-   
+   formRequest : FormGroup;
+   requests : Request[]
+    account : Account
+    departments : Department[]
+    selectOption: string = '0'
     constructor(
         private router :Router,
+        private requestService : RequestServices,
+        private activatedRoute : ActivatedRoute,
+        private accountService : AccountService,
+        private departmentService : DepartmentsServices
         
     ){}
     ngOnInit(): void {
-        
+
+        this.requestService.GetRequets().then(
+            res => {this.requests = res as Request[];},
+            err => {console.log(err)}
+        ),
+        this.departmentService.GetDepartment().then(
+            res => {this.departments = res as Department[];},
+            err => {console.log(err)}
+        )
+        this.formRequest = this.requestService.getFormGroup();
+       this.account = this.accountService.GetAccountLogin()
     }
     
+    createdRequest(){
+
+    }
 }
